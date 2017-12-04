f1371d045071b732653a96c144635c78dd690869
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2017-12-04 11:41:06 +0100

    Fix incorrect path issue and add example to prep_pdb_spores.sh

--- prep_pdb_spores.sh
+++ prep_pdb_spores.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
+# specify pdb file as command line arguments
+# EXAMPLE:
+# prep_pdb_spores.sh *_chain_*.pdb
+
+INPUT_DIR=$(realpath $(dirname $1))
+
+N=30
+(
 for f in "$@"; do
+  ((i=i%N)); ((i++==0)) && wait
   FILE_NAME=$(basename ${f})
-  INPUT_DIR=$(dirname ${f})
   NEW_DIR=$(basename ${f%.*})
-  mkdir ${INPUT_DIR}/${NEW_DIR}
-  cp ${f} ${INPUT_DIR}/${NEW_DIR}/${FILE_NAME}
-  SPORES --mode splitpdb ${INPUT_DIR}/${NEW_DIR}/${FILE_NAME}
+  cd ${INPUT_DIR}
+  mkdir ${NEW_DIR}
+  cp ${f} ${NEW_DIR}/${FILE_NAME}
+  cd ${NEW_DIR}
+  SPORES_linux32bit --mode splitpdb ${FILE_NAME} &
 done
+)
