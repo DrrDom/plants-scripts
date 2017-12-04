d9c9911d38704b63200d8f8a33d9e50e6879ce9b
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2017-12-04 10:43:24 +0100

    Add command line argument to specify pdb files to process in prep_pdb_spores.sh

--- prep_pdb_spores.sh
+++ prep_pdb_spores.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-for f in *_chain*.pdb; do
-  DIR=$(basename ${f%.*})
-  mkdir ${DIR}
-  cp ${f} ${DIR}/${f}
-  SPORES --mode splitpdb ${DIR}/${f}
+for f in "$@"; do
+  FILE_NAME=$(basename ${f})
+  INPUT_DIR=$(dirname ${f})
+  NEW_DIR=$(basename ${f%.*})
+  mkdir ${INPUT_DIR}/${NEW_DIR}
+  cp ${f} ${INPUT_DIR}/${NEW_DIR}/${FILE_NAME}
+  SPORES --mode splitpdb ${INPUT_DIR}/${NEW_DIR}/${FILE_NAME}
 done
