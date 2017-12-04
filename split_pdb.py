acef6db14b09415cdd3c5d417236c694193f7697
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2017-12-04 10:17:05 +0100

    Add option to remove nucleic acid residues while split pdb

--- split_pdb.py
+++ split_pdb.py
@@ -5,12 +5,15 @@ import os
 from collections import defaultdict
 
 
-def main(pdb_fname):
+def main(pdb_fname, remove_na):
     d = defaultdict(list)
     with open(pdb_fname) as pdb:
         for line in pdb:
-            if line[:6] in ['ATOM  ', 'TER   ']:
-                d[line[21]].append(line)
+            if line[:6] in ['ATOM  ', 'TER   ', 'HETATM']:
+                if remove_na and line[17:20].strip() in ['U', 'T', 'G', 'C', 'A']:
+                    continue
+                else:
+                    d[line[21]].append(line)
     for name, lines in d.items():
         with open(os.path.splitext(pdb_fname)[0] + "_chain_%s.pdb" % name, 'wt') as f:
             f.write(''.join(lines))
@@ -19,14 +22,17 @@ def main(pdb_fname):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Split PDB by chains and save to separate PDB files. '
-                                                 'Only ATOM and TER sections are stored.')
+                                                 'Only ATOM, TER and HETATM sections are stored.')
     parser.add_argument('-i', '--in', metavar='input.pdb', required=True,
                         help='input PDB file.')
+    parser.add_argument('-x', '--remove_nucleic_acid', action='store_true',  default=False,
+                        help='remove residues with IDs U, T, C, G, A. Default: false.')
 
     args = vars(parser.parse_args())
     for o, v in args.items():
         if o == "in": pdb_fname = v
+        if o == "remove_nucleic_acid": remove_na = v
 
-    main(pdb_fname=pdb_fname)
+    main(pdb_fname=pdb_fname, remove_na=remove_na)
 
 
