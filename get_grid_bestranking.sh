e3f8c62de4fa50148a9f12531775eca4b068f94c
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2017-12-07 07:30:24 +0100

    Fix get_grid_bestranking.sh to collect bestranking data and path to it

--- get_grid_bestranking.sh
+++ get_grid_bestranking.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # collect bestranking files from all subdirs
 # print dir name + first two fields from bestranking file (mol_name, score)
@@ -6,11 +6,10 @@
 # DIR=$HOME/IMTM/eEF2/u5/dock_plants/grid_dock_01
 
 # find $DIR -name "bestranking.csv" | while read f; do
-find . -name "bestranking.csv" | while read f; do
+find $1 -name "bestranking.csv" | while read f; do
   LOCAL_DIR=`dirname $f`
-  LOCAL_DIR=`basename $LOCAL_DIR`
-#  echo $f
-#  echo $LOCAL_DIR
-#  awk -v var="$LOCAL_DIR" -F"," '{if (NR!=1) {print LOCAL_DIR, $1, $2}}' $f
-  sed "1d" $f | cut -d',' -f1-2 | sed "s/^/${LOCAL_DIR},/"
+#  LOCAL_DIR=`basename $LOCAL_DIR`
+  LOCAL_DIR=$(echo ${LOCAL_DIR} | tr / ,)
+  sed "1d" $f | cut -d',' -f1-2 | awk -v prefix="$LOCAL_DIR" -F"," '{print prefix","$0}'
+#  sed "1d" $f | cut -d',' -f1-2 | sed "s/^/${LOCAL_DIR},/"
 done
