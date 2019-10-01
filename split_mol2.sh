695d14bfe128f051f99d71107008e91eef3a0cf6
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2019-10-01 13:32:35 +0200

    Fixed lost molecules in split_mol2.sh

--- split_mol2.sh
+++ split_mol2.sh
@@ -1,15 +1,19 @@
 #/bin/bash
 
-i=0
+i=-1
 start=1
 chunk=1
-grep -Fn "@<TRIPOS>MOLECULE" $1 | cut -d: -f1 | while read line; do
+
+while read line; do
   ((i++))
   if [ "$i" == $2 ]; then
     end=$(($line - 1))
     sed -n $start,${end}p $1 > ${1%.mol2}_chunk${chunk}.mol2
     start=$line
-    i=1
+    i=0
     ((chunk++))
   fi
-done
+done < <(grep -Fn "@<TRIPOS>MOLECULE" $1 | cut -d: -f1)
+
+# save last chunk
+sed -n $start,'$p' $1 > ${1%.mol2}_chunk${chunk}.mol2
